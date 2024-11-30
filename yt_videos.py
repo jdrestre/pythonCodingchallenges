6c1812775c7773d214100c733940d2f8aebd5d84
jdrestre <jdrestre79@gmail.com>
2024-11-29 21:18:50 -0500

    Descargar videos de Youtube directamente utilizando el link del video

---
+++ yt_videos.py
@@ -0,0 +1,12 @@
+"""Download YouTube videos using Python"""
+
+import yt_dlp
+
+url = input("Enter video url: ")
+
+ydl_opts = {}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
+
+print("Video downloaded successfully!")
