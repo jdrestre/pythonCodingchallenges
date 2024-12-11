d40a1e6914e7190a185d4ef328373582477d6b94
jdrestre <jdrestre79@gmail.com>
2024-12-11 14:38:16 -0500

    Agregar opción para solo sacar los 10 primeros resultados de la búsqueda en google

--- googleSearch.py
+++ googleSearch.py
@@ -1,5 +1,11 @@
+"""
+Realizar búsqueda en Google utilizando python con el módulo googlesearch
+"""
 from googlesearch import search
 query = input("Pregunta cualquier cosa: ")
 
-for url in search(query):
-    print(url)
+for index, url in enumerate(search(query)):
+    if index < 10:
+        print(url)
+    else:
+        break
