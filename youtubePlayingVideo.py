f7111f62296aebf9dc21c324449bea9b66b5134c
jdrestre <jdrestre79@gmail.com>
2024-12-13 22:13:57 -0500

    Actualizando código para poder utilizarlo en WSL 24.04 e incluyendo librerías subprocess, quitando la de webbrowser

--- youtubePlayingVideo.py
+++ youtubePlayingVideo.py
@@ -1,24 +1,47 @@
 """reproducir videos de Youtube utilizando python"""
 
-import webbrowser
-from yt_dlp import YoutubeDL
-
+import yt_dlp
+import subprocess
 
 def search_youtube(query):
-    with YoutubeDL({'quiet': True}) as ydl:
-        result = ydl.extract_info(f"ytsearch:{query}", download=False)[
-            'entries'][0]
-        return result['webpage_url']
+    ydl_opts = {
+        'default_search': 'ytsearch',
+        'max_downloads': 1,
+        'noplaylist': True
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        result = ydl.extract_info(query, download=False)
+        video_url = result['entries'][0]['webpage_url']
+        return video_url
+
+def open_in_browser(url):
+    # Usar el comando de Windows para abrir el navegador desde WSL
+    subprocess.run(['cmd.exe', '/c', 'start', 'msedge', url], check=True)
+
+query = input("Ingresa el nombre de la canción: ")
+video_url = search_youtube(query)
+open_in_browser(video_url)
 
 
-# Solicita al usuario que introduzca el nombre de la canción
-song_name = input("Introduce el nombre de la canción: ")
+# import webbrowser
+# from yt_dlp import YoutubeDL
 
-# Busca y muestra los primeros 10 resultados
-# search_youtube(song_name)
 
-# Busca el video en YouTube
-video_url = search_youtube(song_name)
+# def search_youtube(query):
+#     with YoutubeDL({'quiet': True}) as ydl:
+#         result = ydl.extract_info(f"ytsearch:{query}", download=False)[
+#             'entries'][0]
+#         return result['webpage_url']
 
-# Abre el video en el navegador predeterminado
-webbrowser.open(video_url)
+
+# # Solicita al usuario que introduzca el nombre de la canción
+# song_name = input("Introduce el nombre de la canción: ")
+
+# # Busca y muestra los primeros 10 resultados
+# # search_youtube(song_name)
+
+# # Busca el video en YouTube
+# video_url = search_youtube(song_name)
+
+# # Abre el video en el navegador predeterminado
+# webbrowser.open(video_url)
