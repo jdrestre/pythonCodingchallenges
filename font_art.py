db367d825148cc48e33f6b37b221e7c87590ea7e
jdrestre <jdrestre79@gmail.com>
2024-12-24 19:27:14 -0500

    Agregando la opción del tipo de letra para pyfiglet

--- font_art.py
+++ font_art.py
@@ -1,8 +1,22 @@
+"""
+Imprimir editando el texto con pyfiglet, colorama módulo Fore
+"""
+
 from colorama import Fore
 import pyfiglet
 
-ascii_art = pyfiglet.figlet_format("Feliz Navidad'")
+ascii_art = pyfiglet.figlet_format("Feliz Navidad'", font='dos_rebel')
 print(ascii_art)
 
-font = pyfiglet.figlet_format("Feliz Navidad'")
-print(Fore.RED+font)
+font = pyfiglet.figlet_format("Feliz Navidad'", font='dos_rebel')
+print(Fore.RED + font)
+
+# available_fonts = pyfiglet.FigletFont.getFonts()
+# print(available_fonts)
+
+# FONT_COUNT = 0
+
+# for font in available_fonts:
+#     FONT_COUNT += 1
+
+# print(F"Número de tipos de letra en figlet: {FONT_COUNT}")
