d590835f1dffd847a795504371e96c1c376cc4c3
jdrestre <jdrestre79@gmail.com>
2024-12-26 22:18:39 -0500

    Calcular "Greatest Common Divisor" GCM - "Máximo Común Divisor" MCD sin importar módulo `math`

---
+++ gcd_two_numbers.py
@@ -0,0 +1,18 @@
+"""
+Agregando forma para calcular MCM sin importar módulo `math` a partir del 
+"Greatest Common Divisor" GCM o "Máximo Común Divisor" MCD
+"""
+
+
+def find_gcd(a, b):
+    """ Función para busca GCD """
+    while b:
+        a, b = b, a % b
+    return a
+
+
+number1 = int(input("Ingrese el primer número: "))
+number2 = int(input("Ingrese el segundo número: "))
+
+gcd = find_gcd(number1, number2)
+print(f"The GCD of {number1} and {number2} is {gcd}")
