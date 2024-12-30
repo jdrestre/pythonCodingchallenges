bf2422987b4d38cc54fc1e033f56af243f482151
jdrestre <jdrestre79@gmail.com>
2024-12-30 15:38:10 -0500

    Programa para reemplazar espacios en blanco de un string por guiones `.replace(x, y)`

---
+++ replace_blank_to_hyphen.py
@@ -0,0 +1,9 @@
+"""
+Programa en python para reemplazar espacios en blanco de una cadena por
+guiones
+"""
+
+input_string = input("Enter un texto con espacios en blanco: ")
+output_string = input_string.replace(" ", "-")
+
+print("Texto modificado: ", output_string)
